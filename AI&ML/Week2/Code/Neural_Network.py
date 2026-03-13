cffe90b70aa5e6b943bc3287d3d75db161882807
willy <willyebert@gmx.de>
2026-03-13 18:17:34 +0100

    started neural network implimentation, need to stop to do maths

---
+++ AI&ML/Week2/Code/Neural_Network.py
@@ -0,0 +1,8 @@
+'''Thats my first real Neural Network, i will try to impliment:
+-multiple outputs
+-So that the Components, all recieving the same input data deliver different outputs
+-The exercise is inspired by grokking deep learning'''
+
+weights = [0.3, 0.2, 0.9]
+
+
