52c68e727191202e8f0abf3a3998020f7e9e59dd
willy <willyebert@gmx.de>
2026-03-16 17:26:11 +0100

    implimented a neural network with elementwise multiplication

--- AI&ML/Week2/Code/Neural_Network.py
+++ AI&ML/Week2/Code/Neural_Network.py
@@ -1,8 +1,26 @@
 '''Thats my first real Neural Network, i will try to impliment:
 -multiple outputs
 -So that the Components, all recieving the same input data deliver different outputs
--The exercise is inspired by grokking deep learning'''
+-The exercise is inspired by grokking deep learning
+- Here the goal is using elementwise multiplication'''
 
-weights = [0.3, 0.2, 0.9]
+import numpy as np
 
+weights = np.array([0.3, 0.2, 0.9])
+outputs = ([])
+inputs = np.array([10,11,12])
 
+def NeuralNetwork(inputs, weights, outputs):
+    if len(inputs) != len(weights):
+        return 1
+    else:
+
+     for i in range(len(inputs)):
+      output = 0
+      output = np.dot(weights[i], inputs[i])
+      outputs = np.append(outputs, output)
+    
+    return outputs
+
+outputs = NeuralNetwork(inputs,weights, outputs)
+print(outputs)
