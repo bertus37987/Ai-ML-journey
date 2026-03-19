e96e4faea468b4f0beefc3c071f2eb945da05dcf
willy <willyebert@gmx.de>
2026-03-19 17:50:38 +0100

    developend an neural network in obeject form

--- AI&ML/Week2/Code/neural_network_final_projekt_v1.py
+++ AI&ML/Week2/Code/neural_network_final_projekt_v1.py
@@ -1,10 +1,17 @@
-''' Inspired by the *sentdex* episode'''
+''' Inspired by the *sentdex* episode but i modyfied parts
+for example i wanted to use relu not sigmoid'''
 
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
 #New name for datasets i learnd(inputs for now)
 X = np.array([[3.5, 2.3, 4.5, 6.7],
                    [2.0, 3.0, 5.0, 6.2],
                    [-1.5, 3.4, 6.6, 7.3]])
+
+X, y = spiral_data(100, 3)
 
 
 
@@ -15,17 +22,14 @@ class NeuralNetwork:
         #In this case the shape of bias will alyways be (1, numberofneurons)
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
+        #Relu dierectly intigrated in forward
+        self.output = np.maximum(0, np.dot(inputs, self.weights) + self.biases)
 
 #Why the (4,3) for is the lenght of [0] of X an 3 is the number of neurons you want, not that exist
 #Just to really make the point
-layer1 = NeuralNetwork(4,3)
-layer2 = NeuralNetwork(3,2)
-layer3 = NeuralNetwork(2,5)
-layer4 = NeuralNetwork(5,7)
+#Now im using a real dataset
+
+layer1 = NeuralNetwork(2, 5)
 
 layer1.forward(X)
-layer2.forward(layer1.output)
-layer3.forward(layer2.output)
-layer4.forward(layer3.output)
-print(layer4.output)
+print(layer1.output)
